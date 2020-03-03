8897e60ae44b6cf0654dfd8010298ebbe59689cf
Leopoldine Hallynck <lhallynck@gmail.com>
2020-03-03 18:40:45 +0100

    Initial commit + styling base

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NasaLogo from '../src/Images/NASA_logo.png'
+
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <img src={NasaLogo} className="App-logo" alt="nasalogo" />
+        </div>
+    )
 }
 
-export default App;
+export default App
